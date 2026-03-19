489eff65f9fc3f96464cb1f11800b19cf8ecae60
Lukas Balthazar <LBTurner@outlook.dk>
2026-03-19 19:18:00 +0100

    Initialize FastAPI backend and fix virtual environment setup

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\LBTur\python_projects\python_todo_app_my_version\.venv
+set VIRTUAL_ENV=C:\Users\LBTur\python_projects\python_todo_app_my_version_with_FastAPI\todo_frontend_javascript\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
