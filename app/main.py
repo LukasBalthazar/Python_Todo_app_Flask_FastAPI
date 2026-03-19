5b82dc15814083287f4271756d334d29c16d890f
Lukas Balthazar <LBTurner@outlook.dk>
2026-03-19 15:16:34 +0100

    Added endpoint to create todos

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app =  Flask(__name__)
@@ -14,6 +14,26 @@ def home():
 @app.route('/api/todos', methods=['GET'])
 def get_todos():
     return jsonify(todos)
+
+@app.route('/api/todos', methods=['POST'])
+def add_todo():
+    global todo_id_counter
+
+    data = request.get_json()
+
+    if not data or 'title' not in data:
+        return jsonify({'error': 'Title is required'}), 400
+    
+    new_todo  = {
+        'id': todo_id_counter,
+        'title': data['title'],
+        'completed': data.get('completed', False)
+    }
+
+    todos.append(new_todo)
+    todo_id_counter += 1
+
+    return jsonify(new_todo), 201
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
