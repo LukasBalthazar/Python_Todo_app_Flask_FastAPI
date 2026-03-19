3c208b0d40bd8a059db847319a21430857a38b4a
Lukas Balthazar <LBTurner@outlook.dk>
2026-03-19 19:38:50 +0100

    Added SQLite database and initialize todos table

--- app/main.py
+++ app/main.py
@@ -1,7 +1,30 @@
 from fastapi import FastAPI
+import sqlite3
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "Todo API is running"}
+
+def get_db_connection():
+    conn = sqlite3.connect("todos.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def init_db():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS todos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        completed BOOLEAN NOT NULL DEFAULT 0
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+init_db()
