0044844de72f846f67d3801cb88d4472f9324e80
Lukas Balthazar <LBTurner@outlook.dk>
2026-03-19 14:57:50 +0100

    Added endpoint to fetch all todos

--- app/main.py
+++ app/main.py
@@ -1,12 +1,19 @@
-from flask import Flask 
+from flask import Flask, jsonify
 from flask_cors import CORS
 
 app =  Flask(__name__)
 CORS(app)
 
+todos = []
+todo_id_counter = 1
+
 @app.route('/')
 def home():
     return 'Todo API is running'
+
+@app.route('/api/todos', methods=['GET'])
+def get_todos():
+    return jsonify(todos)
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
