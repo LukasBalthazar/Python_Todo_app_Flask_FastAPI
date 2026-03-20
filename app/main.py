7302dd1e23021b9a37ff98861b432bd95ff17ecd
Lukas Balthazar <LBTurner@outlook.dk>
2026-03-20 15:48:23 +0100

    Added CORS middleware to FastAPI backend

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI, Request, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 import sqlite3
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
